3fa87d5244e140cc5b49696585cd23b110c36467
Xiyang Chen <settinghead@gmail.com>
2014-02-03 22:27:26 -0500

    actively monitors DDP to hide expired nprogress

--- auto-nprogress.js
+++ auto-nprogress.js
@@ -5,70 +5,86 @@ if(Meteor.isClient){
    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
   }
 
-  Meteor._originalSubscribe = Meteor.subscribe;
+  Meteor.startup(function(){
 
-  Meteor.subscribe = function(){
+    Meteor._originalSubscribe = Meteor.subscribe;
 
-    //preserves original onReady and onError functions
-    var newArgs = arguments;
-    var callbacks = {};
+    Meteor.subscribe = function(subscribeName){
 
-    var _this = this;
+      if(subscribeName 
+        && subscribeName.indexOf("meteor.")!==0 
+        && subscribeName.indexOf("meteor_")!==0) {
 
-    function makeFn(fn){
-      return function(){
-        if(document.body) {
-          NProgress.done();
+
+        //preserves original onReady and onError functions
+        var newArgs = arguments;
+        var callbacks = {};
+
+        var _this = this;
+
+        var params = Array.prototype.slice.call(arguments, 1);
+
+        function makeFn(fn){
+          return function(){
+            if(document.body) {
+              NProgress.done();
+            }
+            if(fn){
+              fn.apply(_this, arguments);          
+            }
+          };
         }
-        if(fn){
-          fn.apply(_this, arguments);          
-        }
-      };
-    }
 
-    if(arguments.length > 1) {
-      var lastObj = arguments[arguments.length - 1];
-      if(lastObj) {
-        if(isFunction(lastObj)){
-          callbacks.onReady = makeFn(lastObj);
-          newArgs = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
+        function cut(args){
+          return Array.prototype.slice.call(args, 0, args.length - 1);
         }
-        else{
-          if(lastObj.onReady && isFunction(lastObj.onReady)) {
-            callbacks.onReady = makeFn(lastObj.onReady);
-            newArgs = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
+
+        if(arguments.length > 1) {
+          var lastObj = arguments[arguments.length - 1];
+          if(lastObj) {
+            if(isFunction(lastObj)){
+              callbacks.onReady = makeFn(lastObj);
+              newArgs = cut(arguments);
+            }
+            else{
+              if(lastObj.onReady && isFunction(lastObj.onReady)) {
+                callbacks.onReady = makeFn(lastObj.onReady);
+                newArgs = cut(arguments);
+              }
+
+              if(lastObj.onError && isFunction(lastObj.onError)) {
+                callbacks.onError = makeFn(lastObj.onError);
+                newArgs = cut(arguments);
+              }
+            }
           }
-
-          if(lastObj.onError && isFunction(lastObj.onError)) {
-            callbacks.onError = makeFn(lastObj.onError);
-            newArgs = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
-          }
+        };
+     
+        if(!callbacks.onReady){
+          callbacks.onReady = makeFn();
         }
+        if(!callbacks.onError){
+          callbacks.onError = makeFn();
+        }
+
+        Array.prototype.push.call(newArgs, callbacks);
+
+        if(document.body){
+          NProgress.start();
+          var c = setInterval(function(){
+            if(document.body && DDP._allSubscriptionsReady()) {
+              NProgress.done();
+              clearInterval(c);
+            }
+          }, 80);
+        }
+        var handle = Meteor._originalSubscribe.apply(_this, newArgs);
+        return handle;      
       }
     };
- 
-    if(!callbacks.onReady){
-      callbacks.onReady = makeFn();
-    }
-    if(!callbacks.onError){
-      callbacks.onError = makeFn();
-    }
 
-    Array.prototype.push.call(newArgs, callbacks);
-
-    if(document.body){
-      NProgress.start();
-      setTimeout(function(){
-        if(document.body) {
-          NProgress.done();
-        }
-      }, 20000);
-    }
-    var handle = Meteor._originalSubscribe.apply(_this, newArgs);
-    return handle;      
-  };
-
-  Meteor.withoutBar =  {
-    subscribe: Meteor._originalSubscribe
-  };
+    Meteor.withoutBar =  {
+      subscribe: Meteor._originalSubscribe
+    };
+  });
 }
